38870bb4ed2d267dfa94a8c8cc258cd297377828
GustavoAlfaro3203 <93236897+GustavoAlfaro3203@users.noreply.github.com>
2021-11-03 20:09:22 -0600

    PRACTICAS
    3PRACTICAS

--- VLA/Clase03112021/TAVO.sql
+++ VLA/Clase03112021/TAVO.sql
@@ -1,7 +1,19 @@
-SELECT * FROM 
+ 
+--1- Declare 3 variables , resultado, numero1, numero2 ,luego setear las variables
+--resultado debe guardar la suma de numero1 y numero 2 y luego imprimir el resultado.
 
+--2- Declare 3 variables , resultado, numero1, numero2 , luego setear las variables 
+--resultado debe guardar la multiplicacion de numero1 y numero 2 y luego imprimir el resultado.
+
+--3- Declarar una variable , setearla con el nombre del integrante del grupo que tenga
+--menor edad y imprimir en pantalla.
+
+
+
+Declare @resultado int,@numero1 int,@numero2 intSet @numero1= 5Set @numero2= 2Set @resultado = @numero1 + @numero2Print @resultado
 GO
 
+Declare @resultado int,@numero1 int,@numero2 intSet @numero1= 5Set @numero2= 2Set @resultado = @numero1 * @numero2Print @resultadoGODECLARE@NOMBRE VARCHAR(50)SET @NOMBRE = 'NICOL'PRINT @NOMBRE
 
 
---PRACTICA #1244
+
