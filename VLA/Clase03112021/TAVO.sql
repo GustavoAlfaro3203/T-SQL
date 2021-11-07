946937fa76c369fa7f98dda2c8fe5975ee66691f
GustavoAlfaro3203 <93236897+GustavoAlfaro3203@users.noreply.github.com>
2021-11-07 09:38:41 -0600

    TAREA 3
    TAREA3

--- VLA/Clase03112021/TAVO.sql
+++ VLA/Clase03112021/TAVO.sql
@@ -15,5 +15,66 @@ GO
 
 Declare @resultado int,@numero1 int,@numero2 intSet @numero1= 5Set @numero2= 2Set @resultado = @numero1 * @numero2Print @resultadoGODECLARE@NOMBRE VARCHAR(50)SET @NOMBRE = 'NICOL'PRINT @NOMBRE
 
+GO
 
+--TAREA 3
+
+--1-Cree 11variables , llamada nombre, apellido 1 y 3 , edad , fecha nacimiento , sexo , direccion , correo ,
+--salario , Tiene estudios universitarios? , Seteelas  e imprimalas en pantalla .
+
+DECLARE
+
+@NOMBRE VARCHAR(30),
+@APELLIDO_1 VARCHAR(30),
+@APELLIDO_2 VARCHAR(30),
+@EDAD INT,
+@FECHA_DE_NACIMIENTO DATE,
+@SEXO VARCHAR(1),
+@DIRECCIÓN NVARCHAR(50),
+@CORREO NVARCHAR(50),@SALARIO INT,
+@TIENE_ESTUDIOS_UNIVERSITARIOS VARCHAR(3)
+
+SET @NOMBRE = 'GUSTAVO' 
+SET @APELLIDO_1 = 'ALFARO'
+SET @APELLIDO_2 = 'ROJAS'
+SET @EDAD = 32
+--FATA FECHA DE NACIMIENTO
+SET @SEXO = 'M'
+SET @DIRECCIÓN = 'SAN JOSE'
+SET @CORREO = 'ALPER@DFDF.COM'
+SET @SALARIO = 100
+SET @TIENE_ESTUDIOS_UNIVERSITARIOS = 'SI'
+
+PRINT @NOMBRE
+PRINT @APELLIDO_1
+PRINT @APELLIDO_2 
+PRINT @EDAD
+PRINT @SEXO
+PRINT @DIRECCIÓN
+PRINT @CORREO
+PRINT @SALARIO
+PRINT @TIENE_ESTUDIOS_UNIVERSITARIOS
+GO
+
+--2- Cree un ciclo que imprima 40 veces Hola Mundo .
+
+DECLARE @INICIO INT ,@FINAL INTSET @INICIO =1SET @FINAL= 40WHILE @INICIO <=@FINALBEGINSET @INICIO = @INICIO +1PRINT 'HOLA MUNDO 'END
+GO
+
+
+--Cree 3 variables una de resultado valor1 y valor2 , debe setear y crear una division 
+--y almacenarlo en resultado e imprimir en pantalla.
+
+DECLARE
+
+@RESULTADO INT,
+@VALOR1 INT,
+@VALOR2 INT
+
+
+SET @VALOR1 = 4
+SET @VALOR2 = 2
+SET @RESULTADO= @VALOR1 / @VALOR2
+
+PRINT @RESULTADO
 
